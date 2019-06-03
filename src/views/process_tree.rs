c87118d04303cf6fac94f6e2d153665d1a8297bc
Thomas Bytheway <tb403@cam.ac.uk>
2019-06-03 14:18:24 +0100

    Name view thread

--- src/views/process_tree.rs
+++ src/views/process_tree.rs
@@ -81,81 +81,85 @@ impl View for ProcTreeView {
         let path = params.get_or_def("output", "./proc_tree.json");
         let meta_key = params.get_or_def("meta_key", "cmdline").to_string();
         let mut out = File::create(path).unwrap();
-        let thr = thread::spawn(move || {
-            let mut nodes = HashMap::new();
-            let mut ctx_store: HashMap<ID, CtxNode> = HashMap::new();
-            let mut host_map = HashMap::new();
-            let mut host_count = 0;
-            for tr in stream {
-                match *tr {
-                    DBTr::CreateNode(ref n) | DBTr::UpdateNode(ref n) => match n {
-                        Node::Data(n) if *n.pvm_ty() == PVMDataType::Actor => {
-                            let id = n.get_db_id();
-                            let cmd = n.meta.cur(&meta_key);
-                            if !nodes.contains_key(&id) || neq(&cmd, &nodes[&id]) {
-                                let ctx = ctx_store.get(&n.ctx());
-                                let trace_idx =
-                                    ctx.and_then(|c| c.cont.get("trace_offset")).map(|v| &v[..]);
-                                let ts = ctx.and_then(|c| c.cont.get("time")).map(|v| &v[..]);
-                                let host = ctx.and_then(|c| c.cont.get("host"));
+        let thr = thread::Builder::new()
+            .name("ProcTreeView".to_string())
+            .spawn(move || {
+                let mut nodes = HashMap::new();
+                let mut ctx_store: HashMap<ID, CtxNode> = HashMap::new();
+                let mut host_map = HashMap::new();
+                let mut host_count = 0;
+                for tr in stream {
+                    match *tr {
+                        DBTr::CreateNode(ref n) | DBTr::UpdateNode(ref n) => match n {
+                            Node::Data(n) if *n.pvm_ty() == PVMDataType::Actor => {
+                                let id = n.get_db_id();
+                                let cmd = n.meta.cur(&meta_key);
+                                if !nodes.contains_key(&id) || neq(&cmd, &nodes[&id]) {
+                                    let ctx = ctx_store.get(&n.ctx());
+                                    let trace_idx = ctx
+                                        .and_then(|c| c.cont.get("trace_offset"))
+                                        .map(|v| &v[..]);
+                                    let ts = ctx.and_then(|c| c.cont.get("time")).map(|v| &v[..]);
+                                    let host = ctx.and_then(|c| c.cont.get("host"));
 
-                                let host = if let Some(h) = host {
-                                    if host_map.contains_key(h) {
-                                        Some(host_map[h])
+                                    let host = if let Some(h) = host {
+                                        if host_map.contains_key(h) {
+                                            Some(host_map[h])
+                                        } else {
+                                            host_count += 1;
+                                            host_map.insert(h.clone(), host_count);
+                                            to_writer(
+                                                &mut out,
+                                                &Record::HostVal {
+                                                    uuid: h,
+                                                    idx: host_count,
+                                                },
+                                            )
+                                            .unwrap();
+                                            writeln!(out).unwrap();
+                                            Some(host_count)
+                                        }
                                     } else {
-                                        host_count += 1;
-                                        host_map.insert(h.clone(), host_count);
-                                        to_writer(
-                                            &mut out,
-                                            &Record::HostVal {
-                                                uuid: h,
-                                                idx: host_count,
-                                            },
-                                        )
-                                        .unwrap();
-                                        writeln!(out).unwrap();
-                                        Some(host_count)
-                                    }
-                                } else {
-                                    None
-                                };
+                                        None
+                                    };
 
-                                to_writer(
-                                    &mut out,
-                                    &Record::Node {
-                                        id,
-                                        cmd,
-                                        host,
-                                        trace_idx,
-                                        ts,
-                                    },
-                                )
-                                .unwrap();
-                                writeln!(out).unwrap();
-                                out.flush().unwrap();
-                                nodes.insert(id, cmd.map(|v| v.to_string()));
+                                    to_writer(
+                                        &mut out,
+                                        &Record::Node {
+                                            id,
+                                            cmd,
+                                            host,
+                                            trace_idx,
+                                            ts,
+                                        },
+                                    )
+                                    .unwrap();
+                                    writeln!(out).unwrap();
+                                    out.flush().unwrap();
+                                    nodes.insert(id, cmd.map(|v| v.to_string()));
+                                }
                             }
-                        }
-                        Node::Ctx(n) => {
-                            ctx_store.insert(n.get_db_id(), n.clone());
+                            Node::Ctx(n) => {
+                                ctx_store.insert(n.get_db_id(), n.clone());
+                            }
+                            _ => {}
+                        },
+                        DBTr::CreateRel(ref r) => {
+                            if let Rel::Inf(r) = r {
+                                let src = r.get_src();
+                                let dst = r.get_dst();
+                                if nodes.contains_key(&src) && nodes.contains_key(&dst) {
+                                    to_writer(&mut out, &Record::Edge { src, dst }).unwrap();
+                                    writeln!(out).unwrap();
+                                    out.flush().unwrap();
+                                }
+                            }
                         }
                         _ => {}
-                    },
-                    DBTr::CreateRel(ref r) => {
-                        if let Rel::Inf(r) = r {
-                            let src = r.get_src();
-                            let dst = r.get_dst();
-                            if nodes.contains_key(&src) && nodes.contains_key(&dst) {
-                                to_writer(&mut out, &Record::Edge { src, dst }).unwrap();
-                                writeln!(out).unwrap();
-                                out.flush().unwrap();
-                            }
-                        }
                     }
-                    _ => {}
                 }
-            }
-        });
+            })
+            .unwrap();
         ViewInst {
             id,
             vtype: self.id,
